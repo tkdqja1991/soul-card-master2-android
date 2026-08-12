59500d8493df7380ba3196b0350ba68c31f53519
SCM2 <scm2@local>
2026-08-12 19:50:36 +0900

    Show SCM2 socket write arrays

--- wie_web/src/ts/index.ts
+++ wie_web/src/ts/index.ts
@@ -36,6 +36,9 @@ const installDebugConsoleMirror = () => {
       if (message.includes("SCM2 URL.find:")) {
         const pos = message.indexOf("SCM2 URL.find:");
         addScm2Log(message.slice(pos));
+      } else if (message.includes("SCM2 SOCKET WRITE_ARRAY:")) {
+        const pos = message.indexOf("SCM2 SOCKET WRITE_ARRAY:");
+        addScm2Log(message.slice(pos));
       } else if (message.includes("SCM2 SOCKET WRITE:")) {
         const pos = message.indexOf("SCM2 SOCKET WRITE:");
         addScm2Log(message.slice(pos));
